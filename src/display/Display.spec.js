ae1f5db21b0e6ff2052812435deb09bc653fafda
Chris Cruz <christopherc1331@gmail.com>
2019-10-01 21:08:20 -0700

    Got first test to pass

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from "react";
+import Display from "./Display.js";
+import { render } from "@testing-library/react";
+
+test("Gate default values are correct", () => {
+  const { getByText } = render(<Display />);
+
+  getByText(/Unlocked/i);
+  getByText(/Open/i);
+});
