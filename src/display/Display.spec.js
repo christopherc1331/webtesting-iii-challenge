4dcf611142d6c3db3f0ecfbc26aa7a8c4cd6b696
Chris Cruz <christopherc1331@gmail.com>
2019-10-02 19:19:56 -0700

    Fixed dependancy issue

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from "react";
 import Display from "./Display.js";
+import Controls from "../controls/Controls.js";
 import { render } from "@testing-library/react";
 
 test("Gate default values are correct", () => {
@@ -8,4 +9,11 @@ test("Gate default values are correct", () => {
 
   getByText(/Unlocked/i);
   getByText(/Open/i);
+});
+
+test("cannot be closed or opened if it is locked", () => {
+  const { getByText } = render(<Controls />);
+
+  const lockGateButton = getByText(/Lock Gate/i);
+  const closeGateButton = getByText(/Close Gate/i);
 });
